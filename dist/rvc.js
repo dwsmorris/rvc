84505e44c97358f46560e3ebf5b838f2562c4c89
Darryl Morris <dwsmorris@users.noreply.github.com>
2015-09-26 22:54:07 +0100

    access jsl through namespace

--- dist/rvc.js
+++ dist/rvc.js
@@ -1,9 +1,9 @@
 define([
 	'ractive',
-	"jslToJs"
+	"jsl"
 ], function (
 	Ractive,
-	jslToJs
+	jsl
 ) {
 
   'use strict';
@@ -282,7 +282,7 @@ define([
   				scriptItem = template.splice(i, 1)[0];
 
   				if (item.a && item.a.type && item.a.type === "text/javasclisp") {
-  					var script = jslToJs(scriptItem.f[0]) + ";";
+  					var script = jsl.jslToJs(scriptItem.f[0]) + ";";
 					
   					scriptItem.f[0] = script.replace(/(require\(\"rvc!\w+\"\))?/g, function($0, $1) {
   						return $1 ? $1 + ".prototype.exports" : $0;
